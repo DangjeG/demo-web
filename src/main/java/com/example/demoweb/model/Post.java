385f7ee45aaf402b5b8061e35873ef7f67ee01f2
Dangje <Deplodoctor@gmail.com>
2023-02-12 17:24:04 +0500

    model

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,2 +1,18 @@
-package com.example.demoweb.model;public class Post {
+package com.example.demoweb.model;
+
+public class Post {
+
+    private String text;
+    private Integer likes;
+
+    public Post(String text, Integer likes){
+        this.text = text;
+        this.likes = likes;
+    }
+    public String GetText(){
+        return text;
+    }
+    public Integer GetLikes(){
+        return likes;
+    }
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,2 +1,19 @@
-package com.example.demoweb.service;public class PostService {
+package com.example.demoweb.service;
+
+import com.example.demoweb.model.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostService {
+
+    public List<Post> listAllPosts(){
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post("Hi",0));
+        posts.add(new Post("Hello",0));
+        posts.add(new Post("Bomzhur",0));
+        return posts;
+    }
 }
